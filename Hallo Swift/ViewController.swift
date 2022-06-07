ccb683127f36a61761c5b55b9854025296d6723e
fiqryq <fiqrychoerudin48@gmail.com>
2022-06-07 22:18:22 +0700

    Initial Commit

---
+++ Hallo Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hallo Swift
+//
+//  Created by Fiqry choerudin on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
